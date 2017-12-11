0a71698af87e7ff3c5af304965c1376c6cd92bb0
Sasha Fomina <afomina@stuy.edu>
2017-12-11 13:47:05 -0500

    fib stuff

--- intro.js
+++ intro.js
@@ -2,10 +2,14 @@ var items = document.getElementsByTagName('li');
 var list = document.getElementById("thelist");
 var button = document.getElementById('b');
 var header = document.getElementById('h');
+var b2 = document.getElementById("b2");
+var l2 = document.getElementById("list2");
+var counter = 0; 
+
 
 var adder = function(e){
     var newItem = document.createElement('LI');
-    newItem.innerHTML = 'item ' + items.length;
+    newItem.innerHTML = 'item ' + (items.length - counter);
     list.appendChild(newItem);
     newItem.addEventListener("mouseover" , headChanger);
     newItem.addEventListener("mouseleave" , changeBack);
@@ -26,6 +30,27 @@ var remover = function(e){
     this.remove();
 }
 
+
+var fibonacci = function (n){
+    if(n == 0){
+	return 0;
+    }
+    if (n == 1){
+	return 1;
+    }
+    
+    else {
+	return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+var fibonacciAdd = function(e){
+    var toAppend = document.createElement("LI");
+    toAppend.innerHTML = fibonacci(counter);
+    l2.appendChild(toAppend);
+   counter +=  1;
+}
+
+b2.addEventListener("click", fibonacciAdd);
 button.addEventListener('click', adder);
 
 for (i=0 ; i < items.length ; i++){
